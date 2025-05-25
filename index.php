a849bb89a1e999226e7742bc4377d040011ac330
Owen Liangco <owenliangco21@gmail.com>
2025-05-25 19:10:57 +0800

    feat: implemented section class game-grid

--- index.php
+++ index.php
@@ -1,23 +1,15 @@
 <?php
 require BASE_PATH . '/vendor/autoload.php';
-require BASE_PATH . '/utils/htmlEscape.utils.php';
 
-require_once BASE_PATH . '/handlers/pageData.handler.php';
-require_once BASE_PATH . '/components/cards/note.component.php';
 require_once BASE_PATH . '/components/templates/foot.component.php';
 require_once BASE_PATH . '/components/templates/head.component.php';
 require_once BASE_PATH . '/components/templates/nav.component.php';
 ?>
 
-<?php
-head('Product List');
-navHeader($navList);
-?>
-
-<section class="grid grid-cols-3">
-    <?php displayCardForm($productList); ?>
+<section class="hero">
+  <div class="hero-content">
+    <h1>Welcome to My Website!</h1>
+    <p>Click here to see my archived blog posts.</p>
+    <a href="./pages/index.php" class="cta-button">Archives</a>
+  </div>
 </section>
-
-<?php
-footer();
-?>
