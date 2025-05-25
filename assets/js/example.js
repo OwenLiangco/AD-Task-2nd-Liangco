831724b1049cab81c4d165a43fd8b0704bc25414
Owen Liangco <owenliangco21@gmail.com>
2025-05-25 19:23:45 +0800

    feat: added the js for the burger function

---
+++ assets/js/example.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+const burger = document.getElementById('burger');
+    const nav = document.getElementById('navLinks');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('active');
+    });
+});
